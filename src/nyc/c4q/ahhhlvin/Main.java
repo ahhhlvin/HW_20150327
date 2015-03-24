f612942b6b59df993fd7fb460eb74b1e9f8922a9
ahhhlvin <alvkuang@gmail.com>
2015-03-23 20:20:30 -0400

    initial commit

---
+++ src/nyc/c4q/ahhhlvin/Main.java
@@ -0,0 +1,48 @@
+package nyc.c4q.ahhhlvin;
+/*
+*
+* Access Code 2.1
+* Alvin Kuang
+* 3.23.2015
+*
+*/
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.println("Please enter a word: ");
+        String S = input.next();
+        System.out.println(uniqueCharacters(S));
+
+    }
+
+    // converts entered String into a String of characters that occur only ONCE within the entered String
+    public static String uniqueCharacters(String S) {
+
+
+        S = S.toLowerCase();
+
+        String S2 = "" + S.charAt(0);
+
+        for (int i = 0; i < S.length(); i++) {
+
+            String checkLetter = S.substring(i, i+1);
+
+            if (S2.contains(checkLetter)) {
+                continue;
+            } else {
+                S2 = S2 + checkLetter;
+            }
+        }
+
+
+        return S2;
+
+    }
+}
